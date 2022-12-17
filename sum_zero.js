9103c64f30fcb05277384d548d7acf1cfeba4636
MordecaiO <cai.oladimeji@gmail.com>
2022-12-17 22:45:52 +0000

    Create sum_zero.js

---
+++ sum_zero.js
@@ -0,0 +1,4 @@
+ /*
+write a function called sumZero which takes in a sorted array of integers and finds the first pair of numbers which 
+sum to equal zero. The pair must be returned in an array and if no pair is found return undefined in an array
+*/
