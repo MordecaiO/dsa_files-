6f08064589c965b6277db1f10b87e596fb0f07b2
MordecaiO <cai.oladimeji@gmail.com>
2023-01-08 12:50:32 +0000

    Update Sliding_Window-findLongestSubstring.js

--- Problem_Solving_Patterns/Sliding_Window-findLongestSubstring.js
+++ Problem_Solving_Patterns/Sliding_Window-findLongestSubstring.js
@@ -10,3 +10,32 @@ findLongestSubstring('longestsubstring') // 8
 findLongestSubstring('thisishowwedoit') // 6
 Time Complexity - O(n)
 */
+
+function findLongestSubstring(str){
+  // create chars object to keep track of chars 
+  let obj = {}; let maxLen = 0; 
+   // initialise start and end vars 
+  let start = 0; let end = 0; 
+ 
+  // loop through string 
+  while (end <= str.length){
+      let charStart = str[start]; let charEnd = str[end] ;
+      // if end - start > curr max len 
+      if ((end - start) > maxLen) maxLen = (end - start) ; 
+      console.log("charEnd", charEnd)
+      console.log("obj", obj)
+      // if char in object doesn't exist increment end var and create property in chars obj
+      if (!obj[charEnd]){
+          obj[charEnd] = "I exist, sorry" ; 
+          end++ ; 
+      } else { 
+      // else delete the item at start index from chars var & increment start 
+         delete obj[charStart] ;
+         start++ ; 
+      }
+      
+      
+  }
+  return maxLen
+ 
+}
