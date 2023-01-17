868f385785af35484503ca16ac7e25e6c75a6eff
MordecaiO <cai.oladimeji@gmail.com>
2023-01-17 14:04:06 +0000

    Update isPalindrome.js

--- Recursion/isPalindrome.js
+++ Recursion/isPalindrome.js
@@ -1,2 +1,24 @@
 // Write a recursive function called isPalindrome which returns true if the string passed to it is a palindrome (reads the same forward and backward).
 // Otherwise it returns false.
+
+
+// isPalindrome('awesome') // false
+// isPalindrome('foobar') // false
+// isPalindrome('tacocat') // true
+// isPalindrome('amanaplanacanalpanama') // true
+// isPalindrome('amanaplanacanalpandemonium') // false
+
+function isPalindrome(str){
+     
+    let strArr = str.split(""); 
+    let reversedStrArr = []; 
+    
+    const helper = (helperInput) => { 
+        if (helperInput.length === 0) return ; 
+        reversedStrArr.push(helperInput.slice(-1)[0]) ; 
+        return helper(helperInput.slice(0, -1)) ; 
+    }
+  helper(strArr) ; 
+  
+  return str === reversedStrArr.join("") ? true : false ; 
+}
