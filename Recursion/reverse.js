7fd93edc0e888c73f7c4d7dc87cb5189d93fa3da
MordecaiO <cai.oladimeji@gmail.com>
2023-01-17 13:56:43 +0000

    Update reverse.js

--- Recursion/reverse.js
+++ Recursion/reverse.js
@@ -1,1 +1,21 @@
-// Write a recursive function called reverse which accepts a string and returns a new string in reverse.
+// Write a recursive function called reverse which accepts a string and returns a new string in reverse. 
+
+
+function reverse(str){
+    
+  let strArr = str.split(""); 
+  let reverseStrArr = []; 
+  
+  const helper = (helperInput) => {
+      if (helperInput.length === 0) return ; 
+      reverseStrArr.push(helperInput.slice(-1)[0]) ; 
+      helper(helperInput.slice(0,-1)) ; 
+  }
+  
+  helper(strArr) ; 
+  
+  return reverseStrArr.join(""); 
+} 
+
+// reverse('awesome') // 'emosewa'
+// reverse('rithmschool') // 'loohcsmhtir'
