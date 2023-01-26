01e917410415c529d570206a2af42323aa9c9c63
MordecaiO <cai.oladimeji@gmail.com>
2023-01-26 07:39:23 +0000

    Update bubbleSort.js

--- Sorting Algorithms/bubbleSort.js
+++ Sorting Algorithms/bubbleSort.js
@@ -1,1 +1,24 @@
+// time complexity worst case = n^2 , best case = n 
 
+const bubbleSort = (arr) => {
+  
+    for (let i = arr.length; i > 0 ; i--){
+         let noSwaps = true ; 
+        for (let j = 0; j < (i - 1); j++){
+            console.log(arr, arr[j], arr[j+1])
+            if (arr[j] > arr[j+1]){
+                // SWAP !! 
+                 let temp = arr[j] ; 
+                 arr[j] = arr[j+1] ; 
+                 arr[j+1] = temp ; 
+                noSwaps = false ; 
+            } 
+        }
+
+        if (noSwaps) break 
+    }
+
+    return arr ; 
+}
+
+bubbleSort([35, 1,2,3,4,5,6,7])
