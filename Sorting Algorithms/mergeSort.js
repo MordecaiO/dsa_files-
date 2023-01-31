cf6673e8810af3898239e8666741f1cd9dcd3a39
MordecaiO <cai.oladimeji@gmail.com>
2023-01-31 07:39:31 +0000

    Update mergeSort.js

--- Sorting Algorithms/mergeSort.js
+++ Sorting Algorithms/mergeSort.js
@@ -1,1 +1,41 @@
 
+const mergeSort = (arr)=>{
+
+    const merge = (arr1,arr2)=>{
+        let i = 0
+          , j = 0
+          , result = [];
+        while (i < arr1.length && j < arr2.length) {
+            if (arr1[i] < arr2[j]) {
+                result.push(arr1[i]);
+                i++;
+            } else if (arr1[i] > arr2[j]) {
+                result.push(arr2[j]);
+                j++;
+            } else {
+                result.push(arr1[i], arr2[j]);
+                i++,
+                j++;
+            }
+        }
+
+        while (i < arr1.length) {
+            result.push(arr1[i]);
+            i++;
+        }
+
+        while (j < arr2.length) {
+            result.push(arr2[j]);
+            j++;
+        }
+
+        return result;
+    }
+
+    if (arr.length === 1)
+        return arr;
+    let mid = Math.floor(arr.length / 2);
+    let left = mergeSort(arr.slice(0, mid));
+    let right = mergeSort(arr.slice(mid));
+    return merge(left, right);
+}
